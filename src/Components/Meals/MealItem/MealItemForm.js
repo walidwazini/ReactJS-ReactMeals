2cbf0e8ab91f427b44456dcdbc5508b2cc1e9dae
Walid Wazini <wazini25@gmail.com>
2022-02-01 12:51:31 +0800

    improve input UI

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Input from '../../UI/Input';
 
 import classes from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
   return (
     <form className={classes.form} >
-      <input />
-      <button>Add</button>
+      <Input label='Amount' input={{
+        id: 'amount',
+        type: 'number',
+        min: '0',
+        max: '5',
+        step: '1',
+        defaultValue: '0'
+      }} />
+      <button>+ Add</button>
     </form>
   )
 };
