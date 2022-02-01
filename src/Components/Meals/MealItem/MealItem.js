01b4c00095c81bf82da456218841984752077a44
Walid Wazini <wazini25@gmail.com>
2022-02-01 12:19:45 +0800

    simple input form

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 
 const MealItem = props => {
   const price = `RM ${props.price.toFixed(2)}`
@@ -13,7 +14,7 @@ const MealItem = props => {
         <div className={classes.price} >{props.price} </div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   )
